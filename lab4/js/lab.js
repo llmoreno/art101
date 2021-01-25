988abd78886c00e5bdd07db1ce41634879992356
llmoreno <77122657+llmoreno@users.noreply.github.com>
2021-01-24 16:53:36 -0800

    created lab 4 assignment

---
+++ lab4/js/lab.js
@@ -0,0 +1,11 @@
+//Generator creates unique code for users to connect to.
+    //Code appears in form of link, or typeable form
+//Generator has two input forms, one that accepts the code to connect, one that accepts the word cloud submission.
+//Generator sorts inputted words.
+    //Words that are repeated are combined.
+    //Words with spaces between are treated as separate submissions.
+    //Words with commas are treated as one submission.
+//Generator creates image of submitted words.
+    //Generator assigns random orientation and color to each word, excluding upside down orientation.
+    //Words that were repeated appear larger in the word cloud than words that were typed only once.
+//Generator gives option to save output as image file.
