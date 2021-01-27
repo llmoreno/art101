765b434cb5131ce0deef798f3754d8ffc4c2e840
llmoreno <77122657+llmoreno@users.noreply.github.com>
2021-01-27 14:23:32 -0800

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+//lab5 Javascript
+//Author: Lluvia Moreno
+//Created: 27 2021
+//License: Public Domain
+
+//Define variables
+var name = "Miles";
+var make = "Honda";
+var model = "CRV";
+var year = 2015;
+var currentYear = 2021;
+
+//calculate
+var age = currentYear - year;
+
+//output to web page, must do this or console between each line you want to print
+document.writeln("Car Name: " + name + "<br>");
+document.writeln("Car Make: " + make + "<br>");
+document.writeln("Car Model: " + model + "<br>");
+document.writeln("Car Year: " + year + "<br>");
+document.writeln("Car Age: " + age + "<br>");
